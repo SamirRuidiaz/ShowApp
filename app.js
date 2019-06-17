be379190a19214f84a401b825779af4b2bf6b545
Samir <samirruidiaz@gmail.com>
2019-06-17 02:26:38 -0500

    apply filter app

--- app.js
+++ app.js
@@ -1,51 +1,48 @@
-var page = 0;
-
 var api_key = "f0c595e13a64b16fbf5318fbe6872351";
 var url_base = "https://api.themoviedb.org/3"
 var idioma = "language=es-CO";
 
 angular.module("movies", [])
 .controller("AppController", function($scope, $http) {
+
+   var search = "";
+   $scope.anhos = [
+      {id:"2004"},
+      {id:"2005"},
+      {id:"2006"},
+      {id:"2007"},
+      {id:"2008"},
+      {id:"2009"},
+      {id:"2010"},
+      {id:"2011"},
+      {id:"2012"},
+      {id:"2013"},
+      {id:"2014"},
+      {id:"2015"},
+      {id:"2016"},
+      {id:"2017"},
+      {id:"2018"},
+      {id:"2019"}
+   ];
+
    $http({
       method : "GET",
       url : url_base+"/movie/popular?api_key="+api_key+"&"+idioma+"&page=1"
    }).then(function(response) {
-        $scope.movies = response.data.results;
-
-        console.log( response.data )
+      $scope.movies = response.data.results;
+      //   console.log( response.data )
       //   $scope.page = response.data.page;
       //   $scope.currentPage = response.data.total_pages;
       //   $scope.itemPerPage = response.data.total_results;
 
-      $scope.setItems = function(n){
-         console.log("setItems", n);
-         $scope.itemPerPage = n;
-      };
-
-
-      // $scope.count = 0;
-      $scope.myFunction = function(n) {
-         console.log("setItems", n);
-         // $scope.count++;
-      }
-      
-
-
+      // $scope.setItems = function(n){
+      //    console.log("setItems", n);
+      //    $scope.itemPerPage = n;
+      // };
    }, function myError(response) {
       $scope.movies = response.statusText;
    });
-})
-.controller("anhoController", function($scope, $http) {
-   $http({
-      method : "GET",
-      url : "http://54.210.155.46:8002/api/v1/municipality/"
-   }).then(function(response) {
-      $scope.anhos = response.data;
-   }, function myError(response) {
-      $scope.anhos = response.statusText;
-   });
-})
-.controller("generoController", function($scope, $http) {
+
    $http({
       method : "GET",
       url : url_base+"/genre/movie/list?api_key="+api_key+"&"+idioma
@@ -54,4 +51,39 @@ angular.module("movies", [])
    }, function myError(response) {
       $scope.generos = response.statusText;
    });
+
+   $scope.submitMovies = function() {
+      search = $scope.search.replace(/ /gi, "%20");
+      $http({
+         method : "GET",
+         url : url_base+"/search/movie?api_key="+api_key+"&"+idioma+"&query="+search+"&page=1"
+      }).then(function(response) {
+         $scope.movies = response.data.results;
+         // $scope.generos = response.data.genres;
+      }, function myError(response) {
+         $scope.generos = response.statusText;
+      });
+   }
+
+   $scope.searchGenero = function() {
+      $http({
+         method : "GET",
+         url : url_base+"/discover/movie?api_key="+api_key+"&with_genres="+$scope.selectedGenero
+      }).then(function(response) {
+         $scope.movies = response.data.results;
+      }, function myError(response) {
+         $scope.generos = response.statusText;
+      });
+   }
+
+   $scope.searchanhio = function() {
+      $http({
+         method : "GET",
+         url : url_base+"/discover/movie?api_key="+api_key+"&"+idioma+"&primary_release_year="+$scope.selectedanhio
+      }).then(function(response) {
+         $scope.movies = response.data.results;
+      }, function myError(response) {
+         $scope.generos = response.statusText;
+      });
+   }
 });
--- series/series.js
+++ series/series.js
@@ -5,47 +5,37 @@ var url_base = "https://api.themoviedb.org/3"
 var idioma = "language=es-CO";
 
 angular.module("series", [])
-.controller("AppController", function($scope, $http) {
+.controller("seriesController", function($scope, $http) {
+   var search = "";
+   $scope.anhos = [
+      {id:"2004"},
+      {id:"2005"},
+      {id:"2006"},
+      {id:"2007"},
+      {id:"2008"},
+      {id:"2009"},
+      {id:"2010"},
+      {id:"2011"},
+      {id:"2012"},
+      {id:"2013"},
+      {id:"2014"},
+      {id:"2015"},
+      {id:"2016"},
+      {id:"2017"},
+      {id:"2018"},
+      {id:"2019"}
+   ];
+
    $http({
       method : "GET",
       url : url_base+"/tv/popular?api_key="+api_key+"&"+idioma+"&page=1"
    }).then(function(response) {
-        $scope.movies = response.data.results;
-
-        console.log( response.data )
-      //   $scope.page = response.data.page;
-      //   $scope.currentPage = response.data.total_pages;
-      //   $scope.itemPerPage = response.data.total_results;
-
-      $scope.setItems = function(n){
-         console.log("setItems", n);
-         $scope.itemPerPage = n;
-      };
-
-
-      // $scope.count = 0;
-      $scope.myFunction = function(n) {
-         console.log("setItems", n);
-         // $scope.count++;
-      }
-      
-
-
+      $scope.series = response.data.results;
    }, function myError(response) {
-      $scope.movies = response.statusText;
+      $scope.series = response.statusText;
    });
-})
-.controller("anhoController", function($scope, $http) {
-   $http({
-      method : "GET",
-      url : "http://54.210.155.46:8002/api/v1/municipality/"
-   }).then(function(response) {
-      $scope.anhos = response.data;
-   }, function myError(response) {
-      $scope.anhos = response.statusText;
-   });
-})
-.controller("generoControllertv", function($scope, $http) {
+
+
    $http({
       method : "GET",
       url : url_base+"/genre/tv/list?api_key="+api_key+"&"+idioma
@@ -54,4 +44,38 @@ angular.module("series", [])
    }, function myError(response) {
       $scope.generos = response.statusText;
    });
+
+   $scope.submitSeries = function() {
+      search = $scope.search.replace(/ /gi, "%20");
+      $http({
+         method : "GET",
+         url : url_base+"/search/tv?api_key="+api_key+"&"+idioma+"&query="+search+"&page=1"
+      }).then(function(response) {
+         $scope.series = response.data.results;
+      }, function myError(response) {
+         $scope.series = response.statusText;
+      });
+   }
+
+   $scope.searchGenero = function() {
+      $http({
+         method : "GET",
+         url : url_base+"/discover/tv?api_key="+api_key+"&with_genres="+$scope.selectedGenero
+      }).then(function(response) {
+         $scope.series = response.data.results;
+      }, function myError(response) {
+         $scope.series = response.statusText;
+      });
+   }
+
+   $scope.searchanhio = function() {
+      $http({
+         method : "GET",
+         url : url_base+"/discover/tv?api_key="+api_key+"&"+idioma+"&first_air_date_year="+$scope.selectedanhio
+      }).then(function(response) {
+         $scope.series = response.data.results;
+      }, function myError(response) {
+         $scope.series = response.statusText;
+      });
+   }
 });
