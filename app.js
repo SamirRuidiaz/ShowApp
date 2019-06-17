5d1823797c6840e3920d5c2a24ed0c8d46a2122d
Samir <samirruidiaz@gmail.com>
2019-06-16 21:13:43 -0500

    select con generos

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var page = 0;
 
+var token = "f0c595e13a64b16fbf5318fbe6872351"
+
 angular.module("movies", [])
 .controller("AppController", function($scope, $http) {
    $http({
@@ -8,17 +10,21 @@ angular.module("movies", [])
    }).then(function(response) {
         $scope.movies = response.data;
 
-        $scope.total = $scope.mylist.length;     
+        $scope.total = $scope.movies.length;
+        
+        console.log($scope.total)
         $scope.currentPage = 1;
         $scope.itemPerPage = 2;
         $scope.start = 0;
 
 
       $scope.setItems = function(n){
-            $scope.itemPerPage = n;
+         console.log("setItems", n);
+         $scope.itemPerPage = n;
       };
       // In case you can replace ($scope.currentPage - 1) * $scope.itemPerPage in <tr> by "start"
       $scope.pageChanged = function() {
+         console.log("pageChanged", $scope.itemPerPage);
          $scope.start = ($scope.currentPage - 1) * $scope.itemPerPage;
       }; 
 
@@ -40,18 +46,11 @@ angular.module("movies", [])
 .controller("generoController", function($scope, $http) {
    $http({
       method : "GET",
-      url : "http://54.210.155.46:8002/api/v1/municipality/"
+      url : "https://api.themoviedb.org/3/genre/movie/list?api_key="+token+"&language=en-US"
    }).then(function(response) {
-      $scope.generos = response.data;
+      $scope.generos = response.data.genres;
+      console.log(response.data.genres);
    }, function myError(response) {
       $scope.generos = response.statusText;
    });
-});
-
-//and our filter
-module.filter('offset', function() {
-   return function(input, start) {
-     start = parseInt(start, 10);
-     return input.slice(start);
-   };
- }); 
+}); 
